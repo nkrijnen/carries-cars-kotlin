fd6af0dc3686d0bc42e6fc43d80db30f4597f5fa
Nico Krijnen <nico.krijnen@luminis.eu>
2023-03-19 20:09:39 +0100

    Use kotlin.test imports
    Instead of relying directly on junit 5

--- src/test/kotlin/com/carriesCars/pricingEngine/DurationTest.kt
+++ src/test/kotlin/com/carriesCars/pricingEngine/DurationTest.kt
@@ -1,23 +1,22 @@
 package com.carriesCars.pricingEngine
 
 import com.carriesCars.pricingEngine.Duration.Companion.fromString
-import com.carriesCars.pricingEngine.Duration.Companion.ofMinutes
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class DurationTest {
     @Test
     fun `Duration is at least one minute`() {
-        assertThrows(IllegalArgumentException::class.java) {
+        assertFailsWith<IllegalArgumentException> {
             val lessThan1 = 0L
-            val duration = ofMinutes(lessThan1)
+            Duration.ofMinutes(lessThan1)
         }
     }
 
     @Test
     fun `Converts from and to text`() {
-        val someDuration = ofMinutes(3)
+        val someDuration = Duration.ofMinutes(3)
         assertEquals(someDuration, fromString(someDuration.toString()))
     }
 }
--- src/test/kotlin/com/carriesCars/pricingEngine/PricingEngineTest.kt
+++ src/test/kotlin/com/carriesCars/pricingEngine/PricingEngineTest.kt
@@ -3,8 +3,8 @@ package com.carriesCars.pricingEngine
 import com.carriesCars.pricingEngine.Duration.Companion.ofMinutes
 import org.joda.money.CurrencyUnit
 import org.joda.money.Money
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class PricingEngineTest {
     @Test
